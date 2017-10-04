650f76a488a0b2284e3adbe6c69332056f90bda5
Per Tennebø <tennebo@gmail.com>
2017-10-04 01:47:41 +0100

    Trust PSGallery; remove PSReadline config

--- windows/Configure-First.ps1
+++ windows/Configure-First.ps1
@@ -6,16 +6,16 @@ Basic settings for all development/test boxes.
 # PowerShell
 Write-Host "Setting execution policy..."
 Set-ExecutionPolicy Unrestricted
-Set-PSReadlineOption -EditMode Emacs
-Set-PSReadlineKeyHandler -Key Ctrl+LeftArrow ShellBackwardWord
-Set-PSReadlineKeyHandler -Key Ctrl+RightArrow ShellForwardWord
 
-# OneGet package manager manager
+# PowerShell module repository
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+# OneGet meta-package manager
 Write-Host "Installing Chocolatey and NuGet package providers..."
 Install-PackageProvider -Name Chocolatey
 Install-PackageProvider -Name NuGet
 
-# There is no uninstall; this must be done manually:
+# There is no package provider uninstall; this must be done manually:
 #
 # $pp = Get-PackageProvider -Name Chocolatey
 # $pp.ProviderPath
