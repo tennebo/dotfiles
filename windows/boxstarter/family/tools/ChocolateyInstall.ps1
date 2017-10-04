e32f205b38fad2d53d19e905a1700703b040da24
Per Tennebø <tennebo@gmail.com>
2017-10-04 01:51:53 +0100

    Remove config, leaving apps only

--- windows/boxstarter/family/tools/ChocolateyInstall.ps1
+++ windows/boxstarter/family/tools/ChocolateyInstall.ps1
@@ -1,26 +1,8 @@
-# Configure a clean Windows 10 box to some sane settings.
-# Also install some basic software.
+# Install some basic development software.
 
 $Boxstarter.RebootOk=$true
 $Boxstarter.NoPassword=$false
 $Boxstarter.AutoLogin=$true
-
-# Basic settings
-Disable-InternetExplorerESC
-Disable-BingSearch
-
-# Explorer settings
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions -EnableShowFullPathInTitleBar
-
-# PowerShell (cannot use Set-ExecutionPolicy in the Chocolatey runner)
-Update-ExecutionPolicy RemoteSigned
-
-# OneGet package manager manager
-# Install-PackageProvider -Name Chocolatey
-# Install-PackageProvider -Name NuGet
-
-# Avoid having to confirm each install
-choco feature enable --name allowGlobalConfirmation
 
 # Chocolatey packages to install
 cinst Atom
@@ -29,7 +11,6 @@ cinst notepad2
 cinst notepadplusplus
 cinst openssh
 cinst P4Merge
-cinst poshgit
 cinst visualstudiocode
 
 # Install any Windows updates last
