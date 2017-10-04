09b3743a1fb0195265469de78e33556697661ada
Per Tennebø <tennebo@gmail.com>
2017-10-04 09:58:08 +0100

    Renamed and added comments

--- windows/Microsoft.PowerShell_profile.ps1
+++
@@ -1,9 +0,0 @@
-﻿# This file goes here: $profile
-
-Import-Module posh-git
-
-# Let Control+D to exit the shell
-Set-PSReadlineOption -EditMode Emacs
-Set-PSReadlineOption -HistoryNoDuplicates
-Set-PSReadlineKeyHandler -Key Ctrl+LeftArrow ShellBackwardWord
-Set-PSReadlineKeyHandler -Key Ctrl+RightArrow ShellForwardWord
---
+++ windows/profile.ps1
@@ -0,0 +1,16 @@
+﻿# PowerShell user profile. This file goes in one of these locations:
+#
+# A. Current User, All Hosts:     $profile.CurrentUserAllHosts
+# B. Current User, Current Host:  $profile.CurrentUserCurrentHost
+#
+# For completeness, there are actually six(!) of these profiles:
+#   https://blogs.technet.microsoft.com/heyscriptingguy/2012/05/21/understanding-the-six-powershell-profiles/
+
+# Tab completion and colour-coding for Git
+Import-Module posh-git
+
+# Allow using Control+D to exit the shell
+Set-PSReadlineOption -EditMode Emacs
+Set-PSReadlineOption -HistoryNoDuplicates
+Set-PSReadlineKeyHandler -Key Ctrl+LeftArrow ShellBackwardWord
+Set-PSReadlineKeyHandler -Key Ctrl+RightArrow ShellForwardWord
