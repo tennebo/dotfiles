18f50782fce0b1616cb57a41f7ddc599e8ca96ae
Per Tennebø <tennebo@gmail.com>
2017-10-04 01:51:00 +0100

    Remove most config, leaving mainly apps

--- windows/boxstarter/vm/tools/ChocolateyInstall.ps1
+++ windows/boxstarter/vm/tools/ChocolateyInstall.ps1
@@ -5,20 +5,10 @@ $Boxstarter.RebootOk=$true
 $Boxstarter.NoPassword=$false
 $Boxstarter.AutoLogin=$true
 
-# Basic settings
-Disable-InternetExplorerESC
-Disable-BingSearch
-
-# Explorer settings
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions -EnableShowFullPathInTitleBar
-
 # PowerShell (we cannot use Set-ExecutionPolicy in the Chocolatey runner)
 Update-ExecutionPolicy Unrestricted
 Enable-PSRemoting -Force
 Enable-RemoteDesktop
-
-# Avoid having to confirm each install
-choco feature enable --name allowGlobalConfirmation
 
 # Chocolatey packages to install
 cinst Atom
@@ -31,10 +21,8 @@ cinst notepad2
 cinst notepadplusplus
 cinst openssh
 cinst P4Merge
-cinst poshgit
 cinst procexp
 cinst procmon
 cinst rsync
 cinst vim
 cinst visualstudiocode
-
