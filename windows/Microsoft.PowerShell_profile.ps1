afcfe0743e0c2a95623133e40cfdb6a72bb39da5
Per Tennebø <tennebo@gmail.com>
2017-10-04 02:21:03 +0100

    PowerShell profile

---
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+﻿# This file goes here: $profile
+
+Import-Module posh-git
+
+# Let Control+D to exit the shell
+Set-PSReadlineOption -EditMode Emacs
+Set-PSReadlineOption -HistoryNoDuplicates
+Set-PSReadlineKeyHandler -Key Ctrl+LeftArrow ShellBackwardWord
+Set-PSReadlineKeyHandler -Key Ctrl+RightArrow ShellForwardWord
