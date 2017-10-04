dfd807e2e47d41695f0a1be75e2e63b631429a1c
Per Tennebø <tennebo@gmail.com>
2017-10-04 01:49:04 +0100

    Add synopsis

--- windows/boxstarter/Install-Boxstarter.ps1
+++ windows/boxstarter/Install-Boxstarter.ps1
@@ -1,4 +1,7 @@
-# Install BoxStarter
+<#
+.SYNOPSIS
+Install BoxStarter and Chocolatey.
+#>
 
 . { Invoke-WebRequest -UseBasicParsing http://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression
 Get-Boxstarter -Force
