f00ed3a477b8659c334fdd96aef824f3ce9948c5
Per Tennebø <tennebo@gmail.com>
2017-10-04 02:02:33 +0100

    Avoid calling choco; avoid reboot

--- windows/boxstarter/docker/tools/ChocolateyInstall.ps1
+++ windows/boxstarter/docker/tools/ChocolateyInstall.ps1
@@ -1,17 +1,30 @@
-# Configure a clean Windows 10/2016 box to be used as a Docker host
+# Configure a Windows 10/2016 box to be used as a Docker host
 
 $Boxstarter.RebootOk=$true
 $Boxstarter.NoPassword=$false
 $Boxstarter.AutoLogin=$true
 
 # Enable containers
-Enable-WindowsOptionalFeature -Online -FeatureName Containers -All
-
-# Install Docker
-Install-Module -Name DockerMsftProvider -Force
-Install-Package -Name docker -ProviderName DockerMsftProvider -Force
-if (Test-PendingReboot) { Invoke-Reboot }
+Write-BoxstarterMessage 'Enabling Containers'
+Enable-WindowsOptionalFeature -Online -All -FeatureName Containers
 
 # Enable Hyper-V
-choco install -y Microsoft-Hyper-V-All -source WindowsFeatures
-if (Test-PendingReboot) { Invoke-Reboot }
+Write-BoxstarterMessage 'Enabling Hyper-V'
+Enable-WindowsOptionalFeature -Online -All -FeatureName Microsoft-Hyper-V
+
+# Docker
+Write-BoxstarterMessage 'Installing Docker'
+
+# Install the Microsoft Docker provider, then install Docker itself
+Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+Install-Package -Name docker -ProviderName DockerMsftProvider -Force
+
+Install-PackageProvider ContainerImage -Force
+
+# PowerShell tab completion for Docker
+Write-BoxstarterMessage 'Installing posh-docker module'
+Install-Module -Name posh-docker
+
+if (Test-PendingReboot) {
+    Write-BoxstarterMessage 'Reboot will be required'
+}
