4071c7aa44191401c5d5d702445ba8cc35af902c
Per Tennebø <ptennebo@numerix.com>
2017-10-08 13:43:09 +0100

    Vim colours

---
+++ .vim/colors/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Pull some vim color files from Github
+
+wget https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
+wget https://raw.githubusercontent.com/Lokaltog/vim-distinguished/develop/colors/distinguished.vim
+wget https://raw.githubusercontent.com/garybernhardt/dotfiles/master/.vim/colors/grb256.vim
+wget https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
