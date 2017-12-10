47686cd8d16bfbf64360af759ff8b7e6ab5b759a
Per Ola Tennebø <tennebo@gmail.com>
2017-12-10 18:41:22 +0100

    Configure Git with P4Merge on Mac OS X

---
+++ macosx/config-git-p4.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Configure Git to use P4Merge for diff and merge.
+
+git config --global diff.tool p4merge
+git config --global merge.tool p4merge
+
+# Point to the P4Merge executable
+p4m_exe=/Applications/p4merge.app/Contents/MacOS/p4merge 
+git config --global mergetool.p4merge.path "$p4m_exe"
+git config --global difftool.p4merge.path  "$p4m_exe"
+
