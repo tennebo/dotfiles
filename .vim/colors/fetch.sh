17c3da1cd4e10c9726b1814ead5318102abf14e2
Per Tennebø <tennebo@gmail.com>
2018-01-06 17:22:08 +0000

    Use curl instead of wget

--- .vim/colors/fetch.sh
+++ .vim/colors/fetch.sh
@@ -2,7 +2,7 @@
 #
 # Pull some vim color files from Github
 
-wget https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
-wget https://raw.githubusercontent.com/Lokaltog/vim-distinguished/develop/colors/distinguished.vim
-wget https://raw.githubusercontent.com/garybernhardt/dotfiles/master/.vim/colors/grb256.vim
-wget https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
+curl -O https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
+curl -O https://raw.githubusercontent.com/Lokaltog/vim-distinguished/develop/colors/distinguished.vim
+curl -O https://raw.githubusercontent.com/garybernhardt/dotfiles/master/.vim/colors/grb256.vim
+curl -O https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
