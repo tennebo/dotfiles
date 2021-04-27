dfc774f5348c7904d0f262929cf1732bd2b14ee9
Per Tennebø <tennebo@gmail.com>
2021-04-27 13:48:52 -0400

    Add script to uninstall Python

---
+++ python/remove.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo apt remove -y build-essential libssl-dev libffi-dev
+sudo apt remove -y python3-pip
+sudo apt remove -y python3-dev
+sudo apt remove -y python3-venv
+sudo apt remove -y python3
+
